8c53abba751e662e68f6618604e2a420a0c21c95
Dafydd Francis <daveordref@gmail.com>
2011-03-23 02:47:32 +0000

    Started mangling the html so that it'll link properly

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'haml'
 require 'open-uri'
+require 'nokogiri'
 
 set :haml, :format=>:html5
 
@@ -18,5 +19,24 @@ post '/proxy' do
 end
 
 get '/get_page/*' do
-  URI.parse(params[:splat][0] + request.query_string.gsub("|", "%7C")).read
+  url = params[:splat][0]
+  url = "http://#{url}" if !is_url?(url)
+  parsed_url = URI.parse(url)
+  doc = Nokogiri(open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}"))
+  doc = mangle('img', 'src', parsed_url.host, doc)
+  doc = mangle('link', 'href', parsed_url.host, doc)
+  doc = mangle('a', 'href', parsed_url.host, doc)
+  doc = mangle('form', 'action', parsed_url.host, doc)
+  doc.to_html
+end
+
+def is_url?(url)
+  !(URI.parse(url).scheme.nil?)
+end
+
+def mangle(tag, attribute, prefix, doc)
+  doc.css(tag).each do |e|
+    e[attribute] = "http://#{prefix}/#{e[attribute]}" if !is_url?(e[attribute])
+  end
+  doc
 end
