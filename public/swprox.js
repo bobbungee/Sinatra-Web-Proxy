c28fceac86fb9c711225807cd913b83bc6dc71ef
Dafydd Francis <daveordref@gmail.com>
2011-04-04 11:51:01 +0100

    Made the url box look a bit better and a bit more obvious

--- public/swprox.js
+++ public/swprox.js
@@ -2,13 +2,5 @@ $(document).ready(function(){
   $('#swp_form').animate({
     left: "-380"
   })
-  .hover(function(){
-    $(this).animate({
-      left: "0"
-    });
-  },function(){
-    $(this).animate({
-      left: "-380"
-    });
-  });
+  .hover(function(){$(this).animate({left: "0"});},function(){$(this).animate({left: "-380"});});
 });
