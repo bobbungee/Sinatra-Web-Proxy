f0f750263ff6e6d5ec4d3b0564a60bee1ae21fed
Dafydd Francis <daveordref@gmail.com>
2011-04-04 09:04:39 +0100

    Removed some lines of code and some redundant files

--- main.rb
+++ main.rb
@@ -10,29 +10,13 @@ get '/' do
   haml :index
 end
 
-get '/main.css' do
-  sass :main
-end
-
-get '/proxy.css' do
-  sass :proxy
-end
-
-get '/form/*' do
-  "<base target='_top'>"+(haml :url_form)
-end
-
-post '/proxy' do
-  haml :framed, :layout => :lay_frames
-end
+# Stylesheets
+get('/main.css'){sass :main}
+get('/proxy.css'){sass :proxy}
 
 get '/proxy' do
   @site = mangle_page params[:url], '/proxy?url='
-  haml :unframed, :layout => false
-end
-
-get '/get_page/*' do
-  mangle_page(params[:splat][0], '/get_page/')
+  haml :proxy, :layout => false
 end
 
 def mangle_page(url, prefix)
