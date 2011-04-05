bb39d05ad470d63cdc22a29e501cac7a7d4efccb
Dafydd Francis <daveordref@gmail.com>
2011-04-05 18:38:05 +0100

    Made a new sidebar which doesn't bounce and is a little less ugly

--- public/swprox.js
+++ public/swprox.js
@@ -1,6 +1,20 @@
+var isExtended = 0;
+
 $(document).ready(function(){
   $('#swp_form').animate({
     left: "-380"
   })
   .hover(function(){$(this).animate({left: "0"});},function(){$(this).animate({left: "-380"});});
+  $("#swp_sidebarContents").animate({"width":"toggle"});
+  $("#swp_sidebarTab").click(function(){
+    $("#swp_sidebarContents").animate({ "width":"toggle"},{duration:2000,queue:false
+  });
+  if(isExtended==0){
+      $('#swp_sidebarTab').html($('#swp_sidebarTab').html().replace(/(\.[^.]+)$/, '-active$1'));
+      isExtended++;
+    }else{
+      $('#swp_sidebarTab').html($('#swp_sidebarTab').html().replace(/-active(\.[^.]+)$/, '$1'));
+      isExtended=0;
+    }
+  });
 });
